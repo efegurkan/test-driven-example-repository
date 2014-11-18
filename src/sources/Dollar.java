4e694a6c2d56454a19bde8911ec0284efa979ace
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2014-11-18 21:12:27 +0200

    refactoring the code
    we find duplications between test and code. Both have constants. So we
    refactor it to remove duplication.

--- src/sources/Dollar.java
+++ src/sources/Dollar.java
@@ -2,13 +2,13 @@ package sources;
 
 public class Dollar {
 	
-	public int amount = 10;
+	public int amount;
 	public Dollar(int amount){
-		
+		this.amount = amount;
 	}
 	
 	public void times(int multiplier){
-		
+		amount = amount*multiplier;
 	}
 
 }
