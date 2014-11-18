be7d6fcc4082a2a6a6c776f17bdd5e2d431366a3
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2014-11-18 22:47:56 +0200

    what happens if we compare francs with dollars?

--- src/tests/MoneyExampleTest.java
+++ src/tests/MoneyExampleTest.java
@@ -26,6 +26,7 @@ public class MoneyExampleTest {
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertFalse(new Franc(5).equals(new Dollar(5)));
 		//This test contains duplication
 	}
 	
