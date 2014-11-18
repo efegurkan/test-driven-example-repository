81456c32a901f3a9befb53977dec3bd55442e34c
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2014-11-18 21:02:22 +0200

    Make test compile (still failing)

---
+++ src/sources/Dollar.java
@@ -0,0 +1,14 @@
+package sources;
+
+public class Dollar {
+	
+	public int amount;
+	public Dollar(int amount){
+		
+	}
+	
+	public void times(int multiplier){
+		
+	}
+
+}
--- src/tests/MoneyExampleTest.java
+++ src/tests/MoneyExampleTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import sources.Dollar;
+
 public class MoneyExampleTest {
 
 	@Test
