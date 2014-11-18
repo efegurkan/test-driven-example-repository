399bfc3d6a02bd1bc864ffd3cc7b4b3b9c6b7d20
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2014-11-18 23:14:57 +0200

    change franc also

--- src/sources/Money.java
+++ src/sources/Money.java
@@ -13,5 +13,9 @@ public abstract class Money {
 	public static Money dollar(int amount){
 		return new Dollar(amount);
 	}
+	
+	public static Money franc(int amount){
+		return new Franc(amount);
+	}
 
 }
